7f334e75c1aec365abb54b98947557abc38b9593
Steven Kalt <SKalt@users.noreply.github.com>
2017-07-15 10:05:25 -0400

    Create prototype of hack-the-planet
    With the idea of looking through all available commands for ones that might be called to control Earth (sounds great, right?)

---
+++ hack-the-planet.js
@@ -0,0 +1,14 @@
+uglyRedBook = []; // structure as markdown
+function lookInto(pathArr){
+  try {
+    let result = window;
+    for (let path of pathArr){
+      result = result[path];
+    }
+    uglyRedBook.push(pathArr.join('.'))
+    Object.keys(result).forEach((subitem)=>{
+    if (subdir != '__proto__'){
+      lookInto(pathArr.concat(subitem))
+  } catch(err){}
+}
+console.log(uglyRedBook.join('\n'))
