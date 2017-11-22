a2f229c2ea785b562c03168f08dc9efeecd2101b
Steven Kalt <SKalt@users.noreply.github.com>
2017-11-22 14:56:58 -0500

    Create find-globals.js

---
+++ find-globals.js
@@ -0,0 +1,15 @@
+function getWindowSpecificNames(){
+  iframe = document.createElement('iframe');
+  iframe.style.display = 'none';
+  document.body.appendChild(iframe);
+  const nonStandardWindowProperties = Object.keys(window)
+    .filter(name => !iframe.contentWindow.hasOwnProperty(name))
+    .map(
+      name => Object.assign(
+        {name},
+        Object.getOwnPropertyDescriptor(window, name)
+      )
+    )
+  document.body.removeChild(iframe);
+  return nonStandardWindowProperties;
+}
